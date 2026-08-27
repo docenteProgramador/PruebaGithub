011ec07898decbbebf122e1798b284528c095ca0
CarlosM-08 <carlos_murillo1@unitec.edu>
2026-08-26 18:31:49 -0600

    Actualización Ingresar valor de variable

--- src/ejerciciopruebagithub/EjercicioPruebaGitHub.java
+++ src/ejerciciopruebagithub/EjercicioPruebaGitHub.java
@@ -4,6 +4,7 @@
  */
 package ejerciciopruebagithub;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -18,10 +19,13 @@ public class EjercicioPruebaGitHub {
     public static void main(String[] args) {
         // TODO code application logic here
         //Este es un codigo de prueba de GitHub...Oscar Estuvo Aqui!!!        
-        
+        Random rd = new Random();
         Scanner input = new Scanner(System.in);
         
         int variable1 = 0; //Mensaje de Carlos Murillo
+        
+        System.out.print("Ingrese el valor de la primera variable: ");
+        variable1 = input.nextInt();
     }//Fin de Main
     
 }//Fin de Class
