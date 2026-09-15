ac33d177940daa2912d0214bebbd83d0a6902387
jaasielf26 <jaasiel383@gmail.com>
2026-09-15 10:44:47 -0600

    Actualización de Proyecto

--- EjercicioPruebaGitHub/src/ejerciciopruebagithub/EjercicioPruebaGitHub.java
+++ EjercicioPruebaGitHub/src/ejerciciopruebagithub/EjercicioPruebaGitHub.java
@@ -4,6 +4,8 @@
  */
 package ejerciciopruebagithub;
 
+import java.util.Scanner;
+
 /**
  *
  * @author jaasi
@@ -14,10 +16,71 @@ public class EjercicioPruebaGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-         System.out.println("Hola, GitHub");
-         System.out.println("Mi nombre es Jaasiel");
-         System.out.println("solo estoy probando");
-    }
+        
+        Scanner sc = new Scanner(System.in);
+        
+         int opcion = 0;
+
+        // Constante del impuesto sobre ventas
+        final double IMPUESTO = 0.15;
+
+        do {
+
+            MostrarMenuPrincipal();
+
+            System.out.print("Seleccione una opcion: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+
+                case 1:
+                    System.out.println("\nSelecciono: Realizar Venta");
+                    break;
+
+                case 2:
+                    System.out.println("\nSelecciono: Buscar Producto");
+                    break;
+
+                case 3:
+                    System.out.println("\nSelecciono: Consultar Inventario");
+                    break;
+
+                case 4:
+                    System.out.println("\nSelecciono: Reporte de Ventas");
+                    break;
+
+                case 5:
+                    System.out.println("\n=================================");
+                    System.out.println("      SISTEMA DE VENTAS");
+                    System.out.println("=================================");
+                    System.out.println("Programa finalizado.");
+                    System.out.println("Gracias por utilizar el sistema.");
+                    break;
+
+                default:
+                    System.out.println("\nOpcion incorrecta.");
+                    System.out.println("Ingrese una opcion del 1 al 5.");
+                    break;
+
+            }//Fin Switch
+
+        } while (opcion != 5);
+        
+    }//Fin de Main
     
-}
+     public static void MostrarMenuPrincipal() {
+
+        System.out.println("\n========================================");
+        System.out.println("       SISTEMA DE VENTAS");
+        System.out.println("          FERRETERIA");
+        System.out.println("========================================");
+        System.out.println("1. Realizar Venta");
+        System.out.println("2. Buscar Producto");
+        System.out.println("3. Consultar Inventario");
+        System.out.println("4. Reporte de Ventas");
+        System.out.println("5. Salir");
+        System.out.println("========================================");
+
+    }//Fin de Funcion MostrarMenuPrincipal
+    
+}//Fin de class
