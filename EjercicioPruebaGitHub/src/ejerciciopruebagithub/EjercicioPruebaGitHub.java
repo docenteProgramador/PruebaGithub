b179fc9f224e9937d51c7d93071be3546edf1bc4
jaasielf26 <jaasiel383@gmail.com>
2026-09-15 10:07:15 -0600

    actualizacion de prueba

---
+++ EjercicioPruebaGitHub/src/ejerciciopruebagithub/EjercicioPruebaGitHub.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciopruebagithub;
+
+/**
+ *
+ * @author jaasi
+ */
+public class EjercicioPruebaGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("Hola, GitHub");
+         System.out.println("Mi nombre es Jaasiel");
+         System.out.println("solo estoy probando");
+    }
+    
+}
